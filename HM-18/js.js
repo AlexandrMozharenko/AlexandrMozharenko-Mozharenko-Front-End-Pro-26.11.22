a78c20ced8f78372b19bc616abc51a79373670ac
Alex Mozharenko <a.mozharenko@gmail.com>
2023-01-08 18:19:30 +0200

    change code realisation

--- HM-18/js.js
+++ HM-18/js.js
@@ -2,45 +2,38 @@
 
 const taskOne = document.getElementById('task-one')
 
-const onHoverAdd = () => {
+const onFocusAdd = () => {
     const divInf = document.createElement ('div');
     divInf.innerHTML = '<b>Lorem Ipsum</b> — это просто текст-пустышка полиграфической и наборной индустрии. Lorem Ipsum был стандартным фиктивным текстом в отрасли с 1500-х годов, когда неизвестный печатник взял гранку шрифта и перемешал ее, чтобы сделать книгу образцов шрифтов. Он пережил не только пять столетий, но и скачок в электронный набор текста, оставаясь практически неизменным. Он был популяризирован в 1960-х годах с выпуском листов Letraset, содержащих отрывки Lorem Ipsum, а совсем недавно - с программным обеспечением для настольных издательских систем, таким как Aldus PageMaker, включая версии Lorem Ipsum.'
     divInf.className = 'lorem-inform';
-
     taskOne.appendChild(divInf)
 }
 
-const outHoverDel = () => {
+const outFocusDel = () => {
     const divInfCont = document.getElementsByClassName('lorem-inform');
     for (let i = 0; i <= divInfCont.length; i++) {
         divInfCont[i].remove()
     }
 }
 
-const loremIpsAdd = document.getElementById('text-hover');
-loremIpsAdd.onmouseover = onHoverAdd;
+const loremIpsAdd = document.getElementById('text-focus');
+loremIpsAdd.onfocus = onFocusAdd;
 
-const loremIpsDel = document.getElementById('text-hover')
-loremIpsDel.onmouseleave = outHoverDel;
+const loremIpsDel = document.getElementById('text-focus')
+loremIpsDel.onblur = outFocusDel;
 
 // Task 2
 const onClickAddLink = () => {
     const recieveLink = prompt('Введіть ваше посилання');  
 
     let clearLink;
-    if (recieveLink.search("http"|| "https") === -1) {
+    if (recieveLink.search( /http[s]?/) === -1) {
         clearLink = ("https://" + recieveLink);    
     } else {
         clearLink = recieveLink
     }
-
-    const onClickJump = (clearLink) => {
-        let link = document.getElementById('link');
-        link.setAttribute('href', clearLink);
-    }
-    
-    const jumpToSiteBtn = document.getElementById ('jump-to-site');
-    jumpToSiteBtn.onclick = onClickJump(clearLink);
+    let link = document.getElementById('link');
+    link.setAttribute('href', clearLink);
 }
 
 const addLinkBut = document.getElementById ('add-link');
@@ -59,9 +52,9 @@ const carPhotos = [
     '9.jpg',
 ]
 const showImage = () => {
-    let ranValue = Math.floor(Math.random()* carPhotos.length);
+    const ranValue = Math.floor(Math.random()* carPhotos.length);
     console.log(ranValue);
-    let img = carPhotos[ranValue]
+    const img = carPhotos[ranValue]
     console.log(img)
     const containerImage = document.getElementById('image-shover').src = `./Task3 Photos/${img}`;
 }
